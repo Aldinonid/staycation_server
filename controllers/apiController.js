09f87a75ec58349b2f5cb2d1d263f1b66a012c6d
aldinonid <aldino.efendi7@gmail.com>
2020-05-25 02:45:28 +0700

    API for Booking, Replace Bank Name to Invoice on bookingTable

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -3,6 +3,7 @@ const Traveler = require("../models/Member");
 const Treasure = require("../models/Activity");
 const Category = require("../models/Category");
 const Bank = require("../models/Bank");
+const Booking = require("../models/Booking");
 
 module.exports = {
   landingPage: async (req, res) => {
@@ -105,5 +106,89 @@ module.exports = {
     } catch (error) {
       res.status(500).json({ message: "Internal server error" });
     }
+  },
+
+  bookingPage: async (req, res) => {
+    //? Retrieve data from user fill in
+    const {
+      itemId,
+      duration,
+      bookingStartDate,
+      bookingEndDate,
+      firstName,
+      lastName,
+      email,
+      phoneNumber,
+      accountHolder,
+      bankFrom,
+    } = req.body;
+
+    //? Logic when user not upload image
+    if (!req.file) {
+      res.status(404).json({ message: "Image not found" });
+    }
+
+    //? Logic when user not fill in mandatory field
+    if (
+      itemId === undefined ||
+      duration === undefined ||
+      bookingStartDate === undefined ||
+      bookingEndDate === undefined ||
+      firstName === undefined ||
+      lastName === undefined ||
+      email === undefined ||
+      phoneNumber === undefined ||
+      accountHolder === undefined ||
+      bankFrom === undefined
+    ) {
+      res.status(404).json({ message: "Please input your information" });
+    }
+
+    //? Retrieve item from what user choose
+    const item = await Item.findOne({ _id: itemId });
+
+    //? Logic if item isn't found
+    if (!item) {
+      res.status(404).json({ message: "Item not Found" });
+    }
+
+    //? Formula to generate the Total, Tax and Invoice Number
+    item.sumBooking += 1;
+    await item.save();
+    let total = item.price * duration;
+    let tax = total * 0.1;
+    const invoice = Math.floor(1000000 + Math.random() * 9000000);
+
+    //? Create user Name as Member
+    const member = await Traveler.create({
+      firstName,
+      lastName,
+      email,
+      phoneNumber,
+    });
+
+    //? Grouping all field for Booking Create
+    const newBooking = {
+      invoice,
+      bookingStartDate,
+      bookingEndDate,
+      total: (total += tax),
+      itemId: {
+        _id: item.id,
+        title: item.title,
+        price: item.price,
+        duration: duration,
+      },
+      memberId: member.id,
+      payments: {
+        proofPayment: `images/${req.file.filename}`,
+        bankFrom: bankFrom,
+        accountHolder: accountHolder,
+      },
+    };
+
+    //? Create booking
+    const booking = await Booking.create(newBooking);
+    res.status(201).json({ message: "Booking Success", booking });
   },
 };
